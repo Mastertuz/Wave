97d97fa6b718c2cb5f1cf9810d06d3ffd5510b49
Mastertuz <89520195+Mastertuz@users.noreply.github.com>
2022-09-08 20:37:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $(".menu a,.logo a").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+     $('.slider-blog__inner').slick({
+          dots:true,
+          arrows:false
+         
+      });
+      
+      $('.menu__btn').on('click', function(){
+          $('.header__top-inner').toggleClass('header__top-inner--active');
+         
+      });
+
+      var mixer = mixitup('.gallery__content');
+});
